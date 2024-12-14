8adc9b419f1eb44bfe0bccc225059a13a22f78b8
yashV0201 <vardhan.yash0201@gmail.com>
2024-12-15 02:04:37 +0530

    made the Tile class immutable, ie, it cannot be modified from outside the package or class, used guava library for making the empty tile Map immutable

--- src/com/chess/engine/board/Tile.java
+++ src/com/chess/engine/board/Tile.java
@@ -1,10 +1,28 @@
 package com.chess.engine.board;
 import com.chess.engine.piece.Piece;
+import com.google.common.collect.ImmutableMap;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public abstract class Tile {
-    int tileCoordinate;
+    protected final int tileCoordinate;
 
-    Tile(int tileCoordinate){
+    private static final Map<Integer,EmptyTile> EMPTY_TILES = createAllPossibleEmptyTiles();
+
+    private static Map<Integer, EmptyTile> createAllPossibleEmptyTiles() {
+        final Map<Integer, EmptyTile> emptyTileMap = new HashMap<>();
+        for(int i=0;i<64;i++){
+            emptyTileMap.put(i, new EmptyTile(i));
+        }
+
+        return ImmutableMap.copyOf(emptyTileMap);
+    }
+
+    public static Tile createTile(final int tileCoordinate, final Piece piece){
+        return piece != null?new OccupiedTile(tileCoordinate,piece) : new EmptyTile(tileCoordinate);
+    }
+    private Tile(int tileCoordinate){
         this.tileCoordinate = tileCoordinate;
     }
 
@@ -12,7 +30,7 @@ public abstract class Tile {
     public abstract Piece getPiece();
 
     public static final class EmptyTile extends Tile{
-        EmptyTile(int coordinate){
+        EmptyTile(final int coordinate){
             super(coordinate);
         }
 
@@ -28,7 +46,7 @@ public abstract class Tile {
     }
 
     public static final class OccupiedTile extends Tile{
-        Piece pieceOnTile;
+        private final Piece pieceOnTile;
         OccupiedTile(int tileCoordinate, Piece pieceOnTile){
             super(tileCoordinate);
             this.pieceOnTile = pieceOnTile;
