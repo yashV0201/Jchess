666dee88789a920e32ca3fd27d2f44f12d8a8e74
yashV0201 <vardhan.yash0201@gmail.com>
2024-12-15 14:10:18 +0530

    made the piece class abstract, added the properties of Piece class like position and alliance, and created a method to find the legal moves on the board. so we created three more classes, alliance , move and board

--- src/com/chess/engine/board/Tile.java
+++ src/com/chess/engine/board/Tile.java
@@ -2,20 +2,21 @@ package com.chess.engine.board;
 import com.chess.engine.piece.Piece;
 import com.google.common.collect.ImmutableMap;
 
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
 public abstract class Tile {
     protected final int tileCoordinate;
 
-    private static final Map<Integer,EmptyTile> EMPTY_TILES = createAllPossibleEmptyTiles();
+    private static final Map<Integer,EmptyTile> EMPTY_TILES_CACHE = createAllPossibleEmptyTiles();
 
     private static Map<Integer, EmptyTile> createAllPossibleEmptyTiles() {
         final Map<Integer, EmptyTile> emptyTileMap = new HashMap<>();
         for(int i=0;i<64;i++){
             emptyTileMap.put(i, new EmptyTile(i));
         }
-
+        //Collections.unmodifiableMap(emptyTileMap); // can use this also
         return ImmutableMap.copyOf(emptyTileMap);
     }
 
@@ -30,7 +31,7 @@ public abstract class Tile {
     public abstract Piece getPiece();
 
     public static final class EmptyTile extends Tile{
-        EmptyTile(final int coordinate){
+        private EmptyTile(final int coordinate){
             super(coordinate);
         }
 
@@ -47,7 +48,7 @@ public abstract class Tile {
 
     public static final class OccupiedTile extends Tile{
         private final Piece pieceOnTile;
-        OccupiedTile(int tileCoordinate, Piece pieceOnTile){
+        private OccupiedTile(int tileCoordinate, Piece pieceOnTile){
             super(tileCoordinate);
             this.pieceOnTile = pieceOnTile;
         }
